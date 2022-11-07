b73ab01b092ffd42401109e0ef2b4e95cb7d44d4
Name <email@example.ru>
2022-11-07 21:10:13 +0300

    Добавьте файлы проекта.

---
+++ AppForHW12/Disclaimer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForHW12
+{
+    class Disclaimer
+    {
+        public static void ShowDisclaimer()
+        {
+            string disclaimer1 = "!!Disclaimer!!";
+            string disclaimer2 = "Все имена, персонажи и события являются вымышленными!";
+            string disclaimer3 = "Любое совпадение с реальными людьми является случайным!";
+            string disclaimer4 = "При написании кода ни один политик не пострадал!";
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (disclaimer1.Length / 2)) + "}", disclaimer1));
+            Thread.Sleep(1500);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (disclaimer2.Length / 2)) + "}", disclaimer2));
+            Thread.Sleep(1500);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (disclaimer3.Length / 2)) + "}", disclaimer3));
+            Thread.Sleep(1500);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (disclaimer4.Length / 2)) + "}", disclaimer4));
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ AppForHW12/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AppForHW12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Disclaimer.ShowDisclaimer();
+            var userList = new List<User>
+            {
+                new User("User1", "Chubais", true),
+                new User("User2", "Soloviev",false),
+                new User("User3", "Prigozhin",true),
+                new User("User4", "Kadyrov", true),
+                new User("User4", "Reindeer herder", false)
+            };
+
+            foreach (var user in userList)
+            {
+                Console.WriteLine("Здравствуйте {0}!", user.Name);
+                if (user.IsPremium == false) { user.ShowAds(); }
+                Console.WriteLine();
+                Thread.Sleep(1500);
+            }
+            Console.ReadKey();
+        }
+    }
+}
